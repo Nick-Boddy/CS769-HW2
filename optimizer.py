8a5dc40dbee52ac1e8fc5a9d80f97e3d056d0b96
Nick <nboddy@wisc.edu>
2024-10-09 13:26:21 -0500

    implemented AdamW, still needs testing

--- optimizer.py
+++ optimizer.py
@@ -38,23 +38,47 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
-
                 # State should be stored in this dictionary
                 state = self.state[p]
 
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                betas = group["betas"]
+                epsilon = group["eps"]
+                weight_decay = group["weight_decay"]
+                beta1 = betas[0]
+                beta2 = betas[1]
+
+                # initialize for t = 0
+                if "t" not in state:
+                    state["t"] = 0                        # time step
+                    state["m"] = torch.zeros_like(p.data) # 1st moment
+                    state["v"] = torch.zeros_like(p.data) # 2nd moment
+
+                t = state["t"]
+                m_t = state["m"]
+                v_t = state["v"]
 
                 # Update first and second moments of the gradients
+                t += 1
+                m_t = beta1 * m_t + (1 - beta1) * grad
+                v_t = beta2 * v_t + (1 - beta2) * grad * grad
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                alpha_t = alpha * torch.sqrt(1 - beta2**t) / (1 - beta1**t)
 
                 # Update parameters
 
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                p.data -= (alpha_t * m_t / (torch.sqrt(v_t) + epsilon) + weight_decay * p.data)
+
+                state["t"] = t
+                state["m"] = m_t
+                state["v"] = v_t
+
+                
 
         return loss
